4a25e3476906fd05f71a46888af2211b0167540d
John Kim <cloudiosx@gmail.com>
2022-01-25 07:51:22 -0500

    Implement decidePolicyFor WKNavigationDelegate method

--- Project4 - Easy Browser/ViewController.swift
+++ Project4 - Easy Browser/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     var webView: WKWebView!
     var progressView: UIProgressView!
+    var websiteToLoadInitially = ["apple.com"]
+    var websitesToLoadAsOptions = ["apple.com", "google.com", "hackingwithswift.com"]
     
     override func loadView() {
         webView = WKWebView()
@@ -45,15 +47,19 @@ class ViewController: UIViewController, WKNavigationDelegate {
         toolbarItems = [progressButton, spacer, refresh]
         navigationController?.isToolbarHidden = false
         
-        let url = URL(string: "https://www.apple.com")!
+//        let url = URL(string: "https://www.apple.com")!
+        let url = URL(string: "https://" + websiteToLoadInitially[0])!
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
     }
     
     @objc func openTapped() {
         let ac = UIAlertController(title: "Open page...", message: nil, preferredStyle: .actionSheet)
-        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
-        ac.addAction(UIAlertAction(title: "google.com", style: .default, handler: openPage))
+//        ac.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+//        ac.addAction(UIAlertAction(title: "google.com", style: .default, handler: openPage))
+        for website in websitesToLoadAsOptions {
+            ac.addAction(UIAlertAction(title: website, style: .default, handler: openPage))
+        }
         ac.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         ac.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
         present(ac, animated: true)
@@ -68,6 +74,20 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        let url = navigationAction.request.url
+        
+        if let host = url?.host {
+            for website in websitesToLoadAsOptions {
+                if host.contains(website) {
+                    decisionHandler(.allow)
+                    return
+                }
+            }
+        }
+        decisionHandler(.cancel)
     }
     
     // KVO method
